35157fcb3b487b7907dbd55546fc94c589c98b95
Martin133458 <perdunstaryj76@gmail.com>
2024-11-06 14:11:47 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+import Header from './assets/components/Header/Header'
+
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
